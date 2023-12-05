bf1cef18df39b65e1c0fe742534e4fe6d1b5efd4
renata-aline <renatalinec@gmail.com>
2023-12-05 18:48:09 -0300

    recurso link to

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes ,Route} from "react-router-dom";
+import Inicio from "./Paginas/Inicio";
+import SobreMim from "./Paginas/SobreMim";
+import Menu from "./Componentes/Menu";
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Menu />
     <Routes>
-      
+      <Route path="/" element={<Inicio/>} />
+      <Route path="/sobremim" element={<SobreMim/>} />
+      <Route path="*" element={<div>Página não encontrada</div>} />
+
+
     </Routes>
     </BrowserRouter>
   )
