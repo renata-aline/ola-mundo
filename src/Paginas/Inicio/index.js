aee36cc4f5697475f9eb1c9634ec4edf4ffe0e79
renata-aline <renatalinec@gmail.com>
2023-12-18 16:21:44 -0300

    rename da pasta post para postcard

--- src/Paginas/Inicio/index.js
+++ src/Paginas/Inicio/index.js
@@ -1,7 +1,7 @@
 import styles from "./Inicio.module.css";
 
 import posts from "json/posts.json";
-import Post from "../../Componentes/Post";
+import Post from "../../Componentes/PostCard";
 
 export default function Inicio() {
   return (
