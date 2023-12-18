85bc08671ba9001e2fc73df4819f12947ed221c4
renata-aline <renatalinec@gmail.com>
2023-12-18 15:17:35 -0300

    desenvolvido página sobre mim

---
+++ src/Componentes/PostModelo/index.js
@@ -0,0 +1,15 @@
+import styles from "./PostModelo.module.css";
+
+export default function PostModelo({children, fotoCapa, titulo}) {
+  return (
+    <article className={styles.PostModeloContainer}>
+      <div
+        className={styles.fotoCapa}
+        style={{ backgroundImage: `url(${fotoCapa})` }}
+      ></div>
+
+      <h2 className={styles.titulo}>{titulo}</h2>
+      <div className={styles.PostModeloConteudoContainer}>{children}</div>
+    </article>
+  );
+}
--- src/Paginas/SobreMim/index.js
+++ src/Paginas/SobreMim/index.js
@@ -1,5 +1,34 @@
-export default function SobreMim(){
-    return(
-        <h1>Sobre mim</h1>
-    )
+import styles from "./SobreMim.module.css";
+
+import PostModelo from "Componentes/PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png";
+import fotoSobreMim from "assets/sobre_mim_foto.png";
+
+export default function SobreMim() {
+  return (
+    <PostModelo fotoCapa={fotoCapa} titulo="Sobre mim">
+      <h3 className={styles.subtitulo}>Ola eu sou o Antônio!</h3>
+      <img src={fotoSobreMim} className={styles.fotoSobreMim} />
+      <p className={styles.paragrafo}>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC
+      </p>
+      <p className={styles.paragrafo}>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC
+      </p>
+      <p className={styles.paragrafo}>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC
+      </p>
+      <p className={styles.paragrafo}>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC
+      </p>
+      <p className={styles.paragrafo}>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC
+      </p>
+    </PostModelo>
+  );
 }
